6b7a3925db403cb2ae791633623d7c145ef681cd
tairobinett <51088091+tairobinett@users.noreply.github.com>
2025-11-09 14:03:06 -0500

    Added generate_map API endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import terrain_generator as tg
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+
 
 app = FastAPI()
 
@@ -15,3 +19,29 @@ app.add_middleware(
 @app.get("/hello")
 def hello_world():
     return {"message": "Hello World!"}
+
+@app.post("/generate_map")
+def generate_map(seed):
+    try:
+        input_seed_int = int(seed)
+        # print(f"input_seed_int value: {input_seed_int}")
+    except ValueError:
+        print("Invalid input.")
+        exit()
+
+    generator = tg.TerrainGenerator(width=30, height=30, seed=input_seed_int, tile_size=128)
+    
+    print("Generating terrain...")
+    generator.generate_terrain(scale=0.1, octaves=4)
+    
+    print("Rendering image...")
+    image = generator.render_to_image(show_grid=True)
+
+    image_byte_array = BytesIO()
+    image.save(image_byte_array, format="PNG")
+    image_byte_array.seek(0)
+    return StreamingResponse(image_byte_array, media_type="image/png")
+    
+    # output_file = "battlemap_test.png"
+    # image.save(output_file)
+    # print(f"Map saved to {output_file}")
